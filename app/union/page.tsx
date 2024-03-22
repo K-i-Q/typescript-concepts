7489a5bef1463d6c39352704850dea2d25e8e51c
Carlos Oliveira <carlos.oli1505@gmail.com>
2024-03-22 09:22:42 -0300

    union types

---
+++ app/union/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+type TPerson = {
+    name: string;
+    age: number | string;
+}
+
+type TAddress = {
+    city: string;
+    zipCode: string | number;
+}
+
+type TCustomer = TPerson & TAddress;
+
+export default function IntersectionPage(){
+    const handlePrintCustomer = () =>{
+        const oneCustomer: TCustomer = {
+            name: 'Carlos',
+            age: '28',
+            city: 'Fazenda Rio Grande',
+            zipCode: 88888
+        }
+        const anotherCustomer: TCustomer = {
+            name: 'Rodrigo',
+            age: 30,
+            city: 'São José dos Pinhais',
+            zipCode: '999999'
+        }
+        console.log({oneCustomer});
+        console.log({anotherCustomer});
+    }
+
+    handlePrintCustomer();
+
+    return (
+        <div>
+            Union Example
+        </div>
+    )
+}
